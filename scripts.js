675483545b115564d1770ba0b56c941c935968bc
perimadson35 <araujoperimadson@gmail.com>
2025-04-15 18:52:47 -0300

    Nova alteração nos codigos HTml, Css e javascript

--- scripts.js
+++ scripts.js
@@ -1,22 +1,62 @@
-let body = document.querySelector("body")
-let imagemTenis = document.querySelector(".imagem-tenis")
-let botaoCarrinho = document.querySelector(".botao-carrinho")
+const imagens = [
+    './img/img-fundo-verde.jpg',
+    './img/img-fundo-rosa.jpg',
+    './img/img-fundo-azul.jpg',
+    './img/img-fundo-verde-mobile.jpg',
+    './img/img-fundo-rosa-mobile.jpg',
+    './img/img-fundo-azul-mobile.jpg',
+    './img/nike1.png',
+    './img/nike2.png',
+    './img/nike3.png'
+];
 
-// document = html
-// querySelector = pegador / selecionador
+imagens.forEach(src => {
+    const img = new Image();
+    img.src = src;
+});
 
-function mudarVisual(cor, imagem, corBotao){
-    imagemTenis.classList.add("troca-efeito")
 
-    body.style.background = cor
-    
-    botaoCarrinho.style.background = corBotao
+let main = document.querySelector("main")
 
-    // contador de tempo
-    setTimeout(() =>{
-        imagemTenis.src = imagem
-        imagemTenis.classList.remove("troca-efeito")
-    }, 500)
+let tenis = document.querySelector(".tenis-nike")
 
-    // 1000 milisegundos = 1segundo
+function mudarVisual(cor) {
+    tenis.classList.add("sobe");
+
+    // Define o caminho base das imagens
+    const fundos = {
+        verde: {
+            desktop: './img/img-fundo-verde.jpg',
+            mobile: './img/img-fundo-verde-mobile.jpg',
+            tenis: './img/nike1.png'
+        },
+        rosa: {
+            desktop: './img/img-fundo-rosa.jpg',
+            mobile: './img/img-fundo-rosa-mobile.jpg',
+            tenis: './img/nike3.png'
+        },
+        azul: {
+            desktop: './img/img-fundo-azul.jpg',
+            mobile: './img/img-fundo-azul-mobile.jpg',
+            tenis: './img/nike2.png'
+        }
+    };
+
+    // Detecta se está no mobile
+    const isMobile = window.innerWidth <= 930;
+
+    // Aplica imagem de fundo correta
+    const imagemFundo = isMobile ? fundos[cor].mobile : fundos[cor].desktop;
+    main.style.backgroundImage = `url(${imagemFundo})`;
+
+    // Troca a imagem do tênis com animação
+    setTimeout(() => {
+        tenis.src = fundos[cor].tenis;
+        tenis.classList.remove("sobe");
+        tenis.classList.add("desce");
+
+        setTimeout(() => {
+            tenis.classList.remove("desce");
+        }, 500);
+    }, 500);
 }
